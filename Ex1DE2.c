376cc3d1221a3a88c36dbc4b8abb6635b7dcf5a6
okayama <okayama@gangshang.local>
2022-02-28 13:33:02 +0900

    Initial commit for the formula (DE2).

---
+++ Ex1DE2.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <gsl/gsl_sf_trig.h>
+#include <Accelerate/Accelerate.h>
+#include "DE_trans.h"
+#include "sigma.h"
+
+#define ALPHA 0.5
+#define DOMD 1.57
+
+double* AllocVec(int dim)
+{
+  double* vec;
+  vec = (double*)calloc(dim, sizeof(double));
+
+  /* Check memory */
+  if (vec == NULL) {
+    free(vec);
+    fprintf(stderr,"Vector was not allocated! \n");
+    exit(1);
+  }
+
+  return vec; /* Return the first address of the allocated memory */
+}
+
+void FreeVec(double* v)
+{
+  free(v);
+}
+
+double f(double t)
+{
+  return M_1_PI * (asin(t) + M_PI_2);
+}
+
+double G(double x)
+{
+  return 0.5*cosh(x)/cosh(M_PI_2 * sinh(x));
+}
+
+double Fapp(double* p, double t, int m)
+{
+  double h = log(2*DOMD*m / ALPHA) / m;
+
+  int j;
+
+  double sum = 0;
+
+  for (j = -m; j <= m; j++) {
+    sum += p[j+m] * gsl_sf_sinc((DE_trans_inv(-1, 1, t) - j*h) / h);
+  }
+
+  return sum;
+}
+
+double eta(double x)
+{
+  return 0.5*(1 + x);
+}
+
+int main()
+{
+  int i, j;
+  int n, N;
+  double t, h;
+  double err, maxerr;
+  double Iast;
+  clock_t start, end;
+  double time;
+  double *J, *g, *c;
+
+  int STEP = 1000;
+
+  for (n = 3; n <= 50; n += 6) {
+    start = clock();
+
+    N = 2*n+1;
+    J = AllocVec(N*N);
+    g = AllocVec(N);
+    c = AllocVec(N);
+
+    h = log(2*DOMD*n / ALPHA) / n;
+
+    Iast = 0;
+    for (i = -n; i <= n; i++) {
+      Iast += G(i*h);
+    }
+    Iast *= h;
+
+    for (i = -n; i <= n; i++) {
+      for (j = -n; j <= n; j++) {
+        J[(i+n)*N + (j+n)] = 0.5 + (double)sigma_orig[1000 + (i - j)];
+      }
+      g[(i+n)] = G(i*h) - 0.5 * Iast * DE_trans_div(-1, 1, i*h);
+    }
+
+    /* c = h J g */
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, h, J, N, g, 1, 0.0, c, 1);
+
+    maxerr = 0;
+    for (i = - STEP + 1; i <= STEP - 1; i++) {
+      t = (double)i / (double)(STEP);
+
+      err = fabs(f(t) - Fapp(c, t, n) - Iast*eta(t));
+      maxerr = fmax(err, maxerr);
+    }
+    end = clock();
+
+    time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("%d\t%e\t%e\n", n, err, time);
+
+    FreeVec(J);
+    FreeVec(g);
+    FreeVec(c);
+  }
+
+  return EXIT_SUCCESS;
+}
